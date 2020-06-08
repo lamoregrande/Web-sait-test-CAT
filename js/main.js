7042e9d1c503d7d76c3ee82829160ecb17d788cd
yuliabatkalova <lamoregrande87@gmail.com>
2020-06-08 08:34:29 +0300

    init commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function(){
+ $('.slider').slick({
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/next.svg" alt="next arrow"></button>',
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prev arrow"></button>',
+    
+    responsive: [
+       {
+          breakpoint: 441,
+          settings: {
+             arrows: false
+
+          }
+
+       }
+    ]
+
+ });
+
+});
+
